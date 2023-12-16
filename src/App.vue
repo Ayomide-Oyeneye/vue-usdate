ced1ad0635b306cec2de66c9e9ca1b9663532a91
Ayomide-Oyeneye <nickbelson11@gmail.com>
2023-12-15 17:37:32 -0800

    usedate-vue

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    {{ value }}
+    <br />
+    Date: {{ date.toString() }}
+    <br />
+    Day: {{ getDay() }}
+    <br />
+    Month: {{ getMonth() }}
+    <br />
+    <input type="text" v-model="input" @input="handleChange" />
+    <button @click="changeTheWorld">Change The World</button>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+  data() {
+    return {
+      input: '',
+      value: 'Helloworld',
+      date: new Date(),
+    };
+  },
+  methods: {
+    handleChange(event) {
+      this.input = event.target.value;
+    },
+    changeTheWorld() {
+      this.value= this.input;
+    },
+    getDay() {
+      return this.date.getDate();
+    },
+    getMonth() {
+      return this.date.getMonth();
+    },
+  },
+  watch: {
+    input(newValue) {
+      console.log('Input changed', newValue);
+    },
+  },
+  };
+  </script>
+  <style>
+  *{
+    font-size: xx-large;
+  }
+</style>
+  
